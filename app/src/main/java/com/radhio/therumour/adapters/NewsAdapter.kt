27b2913ef4766da941044422fe677fd24337ec51
radhio-softifybd <radhio.softifybd@gmail.com>
2022-02-07 13:11:22 +0600

    adapter setup done

---
+++ app/src/main/java/com/radhio/therumour/adapters/NewsAdapter.kt
@@ -0,0 +1,80 @@
+package com.radhio.therumour.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.radhio.therumour.R
+import com.radhio.therumour.models.Article
+import kotlinx.android.synthetic.main.item_artical_preview.view.*
+
+/**
+ * Created by Azmia Hoque Radhio on 2/6/2022.
+ */
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>(
+
+) {
+    inner class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    
+    private val differCallback = object : DiffUtil.ItemCallback<Article>() {
+        override fun areItemsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem.url == newItem.url
+        }
+
+        override fun areContentsTheSame(oldItem: Article, newItem: Article): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    private val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        return ArticleViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_artical_preview,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = differ.currentList[position]
+        holder.itemView.apply {
+            Glide.with(this)
+                .load(article.urlToImage)
+                .centerCrop()
+                .placeholder(R.drawable.ic_all_news)
+                .error(R.drawable.ic_all_news)
+                .transition(DrawableTransitionOptions.withCrossFade(1000))
+                .into(ivArticleImage)
+
+            tvSource.text = article.source.name
+            tvTitle.text = article.title
+            tvDescription.text = article.description
+            tvPublish.text = article.publishedAt
+            setOnClickListener {
+                onItemClickLister?.let {
+                    it(article)
+                }
+            }
+        }
+
+    }
+
+    private var onItemClickLister: ((Article) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (Article) -> Unit) {
+        onItemClickLister = listener
+    }
+
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+}
